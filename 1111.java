4abcd21748ed008b32847427a2f1f6723fc4136a
Murphy-Ho <45764765+Murphy-Ho@users.noreply.github.com>
2019-11-12 14:22:29 +0800

    11

---
+++ 1111.java
@@ -0,0 +1,18 @@
+echo "# ceshi" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/Murphy-Ho/ceshi.git
+git push -u origin master
+echo "# ceshi" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/Murphy-Ho/ceshi.git
+git push -u origin master
+echo "# ceshi" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/Murphy-Ho/ceshi.git
+git push -u origin master
